f0eb005fde2ca77d6553754e2dbbd3103e654683
Stedders <you@example.com>
2023-08-23 05:54:56 +0100

    Improved algorithm, added download button.

--- dashboard.py
+++ dashboard.py
@@ -1,13 +1,17 @@
-from datetime import date, timedelta
+import io
+from datetime import date, timedelta, time, datetime
 
 import pandas as pd
-import polars as pl
-import streamlit as st
 import plotly.express as px
+import streamlit as st
 
-from drunkards_walk import iterative_random_walk, pandas_vectorised_random_walk, polars_vectorised_random_walk
+from drunkards_walk import (
+    iterative_random_walk,
+    pandas_vectorised_random_walk,
+    polars_vectorised_random_walk,
+)
 
-dashboard_name = "Drunkards Walk Dashboard"
+dashboard_name = "Random Walk Dashboard"
 st.set_page_config(
     page_title=dashboard_name,
     page_icon=":beer:",
@@ -27,24 +31,51 @@ algorithms = {
 df = None
 with st.sidebar as sb:
     st.header("Configuration")
-    algorithm = st.radio("Algorithm", ("Iterative", "Pandas Vectorised", "Polars Vectorised"))
+    company_count = st.slider("No. Companies", min_value=0, max_value=200, value=5)
+    start = st.date_input("Start Date", value=date.today() - timedelta(days=2))
+    end = st.date_input("End Date", value=date.today())
+    open = st.time_input("Open Time", value=time(hour=9))
+    close = st.time_input("Close Time", value=time(hour=16, minute=30))
+    interval_minutes = st.number_input(
+        "Interval (mins)", value=2, min_value=1, max_value=20
+    )
+    algorithm = st.radio("Algorithm", algorithms)
 
     if st.button("Generate Data"):
-        df = None
+        df = algorithms[algorithm](
+            company_count=company_count,
+            interval=timedelta(minutes=interval_minutes),
+            timeseries_start=start,
+            timeseries_end=end,
+            open_time=open,
+            close_time=close,
+        ).set_index("timestamp")
 
-#
-# df = iterative_random_walk(
-#     company_count=5,
-#     start=date.today(),
-#     end=date.today() + timedelta(days=5),
-#     interval=timedelta(minutes=1),
-# )
+df: pd.DataFrame | None
 if df is not None:
     st.plotly_chart(
         px.line(
             data_frame=df,
-            x="timestamp",
-            y=[col for col in df.columns if col != "timestamp:w" ""],
+            x=df.index,
+            y=df.columns,
         ),
         use_container_width=True,
+    )
+
+    base_filename = f"{datetime.now().isoformat().replace(':','')}_random_walk"
+    st.download_button(
+        "Download as CSV",
+        df.to_csv().encode("utf-8"),
+        f"{base_filename}.csv",
+        "text/csv",
+    )
+    output = io.BytesIO()
+    writer = pd.ExcelWriter(output)
+    df.to_excel(
+        writer,
+        sheet_name="Random Walk",
+    )
+    writer.close()
+    st.download_button(
+        "Download as Excel", data=output, file_name=f"{base_filename}.xlsx"
     )
--- drunkards_walk.py
+++ drunkards_walk.py
@@ -1,32 +1,37 @@
 import random
-from datetime import date, timedelta, datetime
+from datetime import date, timedelta, datetime, time
 
 import pandas as pd
 import polars as pl
 
 
 def iterative_random_walk(
-        company_count: int,
-        start: date,
-        end: date,
-        interval: timedelta,
-) -> pl.DataFrame:
+    company_count: int,
+    timeseries_start: date,
+    timeseries_end: date,
+    interval: timedelta,
+    open_time: time,
+    close_time: time,
+) -> pd.DataFrame:
     data = {f"company {x}": [100] for x in range(company_count)}
-    timestamp = datetime.combine(start, datetime.min.time())
+    timestamp = datetime.combine(timeseries_start, datetime.min.time())
     timestamps = [timestamp]
-    while timestamp.date() < end:
+    while timestamp.date() <= timeseries_end:
         timestamp += interval
+        print(f"Processing {timestamp}")
+        if timestamp.time() < open_time or timestamp.time() > close_time:
+            continue
+
         for company in data:
             data[company].append(data[company][-1] + random.random())
         timestamps.append(timestamp)
     data["timestamp"] = timestamps
-    return pl.DataFrame(data)
+    return pl.DataFrame(data).to_pandas()
 
 
 def pandas_vectorised_random_walk() -> pd.DataFrame:
     return pd.DataFrame()
 
 
-def polars_vectorised_random_walk() -> pl.DataFrame:
-    return pl.DataFrame()
-
+def polars_vectorised_random_walk() -> pd.DataFrame:
+    return pl.DataFrame().to_pandas()
